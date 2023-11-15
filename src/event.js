2f7b737146fe33273168c9c99c2547036ec16af3
pigpgw <ceh20002@naver.com>
2023-11-15 23:46:01 +0900

    Feat 불필요한 코드 삭제

--- src/event.js
+++ src/event.js
@@ -1,7 +1,3 @@
-import { Random, Console } from '@woowacourse/mission-utils';
-import inputValidation from './inputValidation.js';
-import inputView from './InputView.js';
-import OutputView from './OutputView.js';
 import Menu from './Menu.js';
 
 
