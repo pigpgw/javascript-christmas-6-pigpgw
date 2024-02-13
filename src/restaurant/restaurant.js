1fb5fc86775edf3e3a2f5c031bc0a7bb5120bf51
pigpgw <ceh20002@naver.com>
2024-02-14 03:33:03 +0900

    UPDATE:README.md

---
+++ src/restaurant/restaurant.js
@@ -0,0 +1,5 @@
+class restaurant {
+  constructor() {}
+}
+
+export default restaurant;
--- src/restaurant/restaurantController.js
+++ src/restaurant/restaurantController.js
@@ -1,24 +1,22 @@
-import menu from './Menu.js';
 import InputView from './InputView.js';
 import Validator from './Validator.js';
 import { Console } from '@woowacourse/mission-utils';
-class restaurantController {
-    async run() {
-      try {
-        const visitDay = await this.getUserVisitDay();
-      } catch (error) {
-        console.error('An error occurred:', error.message);
-        await this.run(); 
-      }
-    }
-  
-    async getUserVisitDay() {
-      const input = await InputView.readDate();
-      if (!Validator.isValidVisitDay(Number(input))) {
-        throw new Error('Invalid visit day. Please enter a valid day.');
-      }
-      return input;
-    }
-  }
 
-export default restaurantController;
+class RestaurantController {
+    async run() {
+        try {
+            const visitDay = await this.getUserVisitDay();
+        } catch (error) {
+            Console.print(error.message);
+            await this.run();
+        }
+    }
+
+    async getUserVisitDay() {
+        const input = await InputView.readDate();
+        Validator.isValidVisitDay(Number(input)); // Throws error if invalid
+        return input;
+    }
+}
+
+export default RestaurantController;
