c488db161d844e899d52264c688431cac7bb338a
pigpgw <ceh20002@naver.com>
2024-02-14 02:08:21 +0900

    feat:Add Uteco Restaurant's December event planner message

---
+++ src/restaurant/Constant.js
@@ -0,0 +1,10 @@
+export const MESSAGE = {
+    GREETING: "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.",
+    ASK_ORDER: "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)",
+    ORDER_LIST: "<주문 메뉴>",
+    BEFORE_DISCOUNT: "<할인 전 총주문 금액>",
+    SERVICE_MENU:"<증정 메뉴>",
+    BENEFITS:"<혜택 내역>",
+    AFTER_DISCOUNT:"<할인 후 예상 결제 금액>",
+    EVENT_VEDGE:"<12월 이벤트 배지>"
+}
