45f451e937e2a8275b61297e20aad0edf9c70644
pigpgw <ceh20002@naver.com>
2024-02-14 23:33:52 +0900

    docs: UPDATE READEME.md

--- __tests__/InputOutTest.js
+++ __tests__/InputOutTest.js
@@ -1,27 +1,28 @@
 import Validator from "../src/restaurant/Validator"
-
+import OutputView from "../src/restaurant/OutputView";
 describe('input Test',() => {
     test(('check validator can validate user incorrect input - string'), () => {
         expect(() => {
             !Validator.isValidVisitDay("hi")
-        }).toThrow();
+        }).toThrow("[ERROR]");
     })
 
     test(('check validator can validate user incorrect input - over range day'), () => {
         expect(() => {
             !Validator.isValidVisitDay(32)
-        }).toThrow();
+        }).toThrow("[ERROR]");
     })
 
     test(('check validator can validate user incorrect input - order incorrect menu(not exist)'), () => {
         expect(() => {
             !Validator.isValidateOrder("라멘-1,제로콜라-1")
-        }).toThrow();
+        }).toThrow("[ERROR]");
     })
     
     test(('check validator can validate user incorrect input - order incorrect count(string)'), () => {
         expect(() => {
             !Validator.isValidateOrder("타파스-d,제로콜라-1")
-        }).toThrow();
+        }).toThrow("[ERROR]");
     })
 })
+
